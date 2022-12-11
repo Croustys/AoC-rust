054da7f0111dec3cd338b05f2be9275219407fbc
Croustys <akos.barabas2002@gmail.com>
2022-12-11 20:46:51 +0100

    day05 2/2

--- lib/day05/src/main.rs
+++ lib/day05/src/main.rs
@@ -57,10 +57,54 @@ fn part_one(input: &str) -> String {
     }
     result
 }
+fn part_two(input: &str) -> String {
+    let mut stacks: Vec<VecDeque<u8>> = vec![VecDeque::new(); 9];
+    input
+        .lines()
+        .take(8)
+        .for_each(|l| {
+            let mut elements = [None; 9];
+            for (i, p) in (1..l.len()).step_by(4).enumerate() {
+                let c = l.as_bytes()[p];
+                elements[i] = if c == b' ' { None } else { Some(c) };
+            }
+            for (i, e) in elements
+                .into_iter()
+                .enumerate()
+                .filter(|(_, e)| e.is_some()) {
+                stacks[i].push_back(e.unwrap());
+            }
+        });
+
+    input
+        .lines()
+        .skip(10)
+        .map(Move::from_line)
+        .for_each(|m| {
+            let mut temp_stacks = VecDeque::with_capacity(m.count);
+            for _ in 0..m.count {
+                temp_stacks.push_front(stacks[m.from - 1].pop_front().unwrap());
+            }
+            temp_stacks.iter().for_each(|e| {
+                stacks[m.to - 1].push_front(*e);
+            });
+        });
+
+    let mut result = String::from("");
+    for i in stacks.iter() {
+        if i.len() < 1 {
+            continue;
+        }
+        let ch = i[0] as char;
+        result.push(ch);
+    }
+    result
+}
 
 fn main() {
     let input = include_str!("test.txt");
-    println!("{}", part_one(input));
+    //println!("{}", part_one(input));
+    println!("{}", part_two(input));
 }
 
 #[cfg(test)]
@@ -71,5 +115,10 @@ mod test {
     fn test_one() {
         let input = include_str!("test.txt");
         assert_eq!("CMZ", part_one(input));
+    }
+    #[test]
+    fn test_two() {
+        let input = include_str!("test.txt");
+        assert_eq!("MCD", part_two(input));
     }
 }
